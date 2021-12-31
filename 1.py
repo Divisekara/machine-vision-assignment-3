3f2a9fb42c764fd9eb56f23c5ec0874ce651bb3d
ASITHA INDRAJITH DIVISEKARA <46625806+Divisekara@users.noreply.github.com>
2021-12-31 15:15:55 +0530

    Dataset folder created

---
+++ 1.py
@@ -0,0 +1,13 @@
+import numpy as np
+from typing import Tuple
+
+def load_dataset(src_dir = "C:\Users\Asitha\Desktop\machine vision assignment 3\\170150A"): # -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray]:]
+    np.loadtxt(src_dir)
+
+"""Load the dataset as a set of numpy arrays.
+Args:
+src_dir: Directory where dataset files are stored.
+Returns:
+(x_train, y_train, x_val, y_val, x_test) tuple where each array is one dimensional.
+"""
+pass # Implement here.
